318798fabf953b56f3fb86b34ddeb9b9676730b4
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-02-13 12:26:47 +0400

    feat: add background depands on time

--- momentum/script.js
+++ momentum/script.js
@@ -2,9 +2,13 @@ const time = document.querySelector('.time');
 const dateElement = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const name = document.querySelector('.name');
+const body = document.querySelector('body');
 
 window.addEventListener('beforeunload', setLocalStorage);
-window.addEventListener('load', getLocalStorage);
+window.addEventListener('load', () => {
+    getLocalStorage();
+    body.style.backgroundImage = `url(${setBg()})`;
+});
 //Date and time
 
 function showTime() {
@@ -28,12 +32,12 @@ function showDate() {
 //Greeting
 
 function showGreeting() {
-    const date = new Date();
-    const hours = date.getHours();
-    greeting.textContent = `Good ${getTimeOfDay(hours)}`;
+    greeting.textContent = `Good ${getTimeOfDay()}`;
 }
 
-function getTimeOfDay(hours) {
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
     const arrTimeOfDay = ['night', 'morning', 'afternoon', 'evening',];
     const timeNow = Math.floor(hours / 6);
     return arrTimeOfDay[timeNow];
@@ -48,3 +52,32 @@ function getLocalStorage() {
       name.value = localStorage.getItem('name');
     }
 }
+
+//Picture
+
+function getRandomNum(min, max) {
+    let minN = Math.ceil(min);
+    let maxN = Math.floor(max);
+    return String(Math.floor(Math.random() * (maxN - minN + 1)) + minN).padStart(2, '0');
+}
+
+function setBg() {
+    const timeForPicture = getTimeOfDay();
+    let numPicture;
+    switch (timeForPicture) {
+        case 'night':
+            numPicture = getRandomNum(0, 5);
+            break;
+        case 'morning':
+            numPicture = getRandomNum(6, 10);
+            break;
+        case 'afternoon':
+            numPicture = getRandomNum(11, 15);
+            break;
+        case 'evening':
+            numPicture = getRandomNum(16, 20);
+            break;
+    }
+    return `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeForPicture}/${numPicture}.jpg`;
+}
+
