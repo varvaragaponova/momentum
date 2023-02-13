13d137fab8b96175c0e1e5b2942aa66a1567321f
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-02-13 10:38:36 +0400

    feat: add greeting

--- momentum/script.js
+++ momentum/script.js
@@ -1,7 +1,10 @@
 const time = document.querySelector('.time');
 const dateElement = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
 
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
 //Date and time
 
 function showTime() {
@@ -9,6 +12,7 @@ function showTime() {
     const currentTime = date.toLocaleTimeString();
     time.textContent = `${currentTime}`;
     showDate();
+    showGreeting();
     setTimeout(showTime, 1000);
   }
 
@@ -16,26 +20,31 @@ showTime();
 
 function showDate() {
     const date = new Date();
-    const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
     const currentDate = date.toLocaleDateString('en-US', options);
     dateElement.textContent = `${currentDate}`;
 }
 
 //Greeting
 
-function getTimeNow() {
+function showGreeting() {
     const date = new Date();
     const hours = date.getHours();
-    return hours;
+    greeting.textContent = `Good ${getTimeOfDay(hours)}`;
 }
 
-console.log(getTimeNow());
-
-
-function getTimeOfDay() {
-
+function getTimeOfDay(hours) {
+    const arrTimeOfDay = ['night', 'morning', 'afternoon', 'evening',];
+    const timeNow = Math.floor(hours / 6);
+    return arrTimeOfDay[timeNow];
 }
 
-getTimeOfDay();
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
 
-
+function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+}
